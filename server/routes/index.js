e4adb1c65a2853e909e1640e45c3bee2a07863a0
zhihaoyao <zhihaoyao@zhihaoyaodeMacBook-Pro.local>
2018-09-19 17:00:17 +0800

    init v2

---
+++ server/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.post('/submitUserData', function (req, res, next) {
+    console.log('hello');
+    res.send({key: 'hello'})
+});
+
+module.exports = router;
